7c93af773dafab16fc0662a343d8342727be2637
Emanuel Brito <emanuelguimaraes23@gmail.com>
2024-11-30 21:13:12 -0300

    Implementação da estrutura inicial da API de Pagamento

---
+++ destaxa-api/src/main/java/com/destaxa/destaxa_api/DestaxaApiApplication.java
@@ -0,0 +1,13 @@
+package com.destaxa.destaxa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestaxaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestaxaApiApplication.class, args);
+	}
+
+}
---
+++ destaxa-api/src/test/java/com/destaxa/destaxa_api/DestaxaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destaxa.destaxa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestaxaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

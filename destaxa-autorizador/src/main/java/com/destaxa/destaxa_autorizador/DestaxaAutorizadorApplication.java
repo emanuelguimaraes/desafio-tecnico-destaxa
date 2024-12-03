5beeee244278c20086f00eb3ba45af448f578bf5
Emanuel Brito <emanuelguimaraes23@gmail.com>
2024-12-03 10:31:37 -0300

    Implementação da estrutura inicial da aplicação do Autorizador

---
+++ destaxa-autorizador/src/main/java/com/destaxa/destaxa_autorizador/DestaxaAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package com.destaxa.destaxa_autorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestaxaAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestaxaAutorizadorApplication.class, args);
+	}
+
+}
---
+++ destaxa-autorizador/src/test/java/com/destaxa/destaxa_autorizador/DestaxaAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destaxa.destaxa_autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestaxaAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
